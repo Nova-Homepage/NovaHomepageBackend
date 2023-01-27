3250d63dfefdeba8f5fdb7a706c7d597593ce72b
Double_J <double04j@gmail.com>
2023-01-28 05:08:37 +0900

    Add member method, Change package DTO and Delete Exception Package

--- src/main/java/com/board/novaapi/Dto/MemberDto.java
+++
@@ -1,9 +0,0 @@
-package com.board.novaapi.Dto;
-
-public class MemberDto {
-    private String studentId;
-    private String password;
-    private String email;
-    private String name;
-    private String role;
-}
