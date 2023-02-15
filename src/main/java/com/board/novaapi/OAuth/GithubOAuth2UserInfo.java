05f6c3e004f6ddaab3ac09417f98d9c683aca4fd
Double_J <double04j@gmail.com>
2023-02-15 18:26:43 +0900

    GithubOAuth2UserInfo 주석추가

--- src/main/java/com/board/novaapi/OAuth/GithubOAuth2UserInfo.java
+++ src/main/java/com/board/novaapi/OAuth/GithubOAuth2UserInfo.java
@@ -11,6 +11,7 @@ public class GithubOAuth2UserInfo extends OAuth2UserInfo {
         return ((Integer) attributes.get("id")).toString();
     }
 
+    // 깃허브 이름 과 이메일이 공개가 아닐 경우 반환 값이 없음으로 임의로 not name 및 not email 할당
     @Override
     public String getName() {
         if(attributes.get("name") == null){
